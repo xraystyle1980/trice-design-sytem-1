e040c324c871eeb4eb0c049bd01a0aad7357a39a
Matt Trice <matt@trice.design>
2023-06-30 10:44:57 -0400

    styled components working

--- src/lib/components/Button.js
+++ src/lib/components/Button.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Buttonbase = styled.button`
+background-color: #11e6bf;
+color: white;
+font-family: Courier;
+font-size: 32px;
+border: none;
+padding: 16px 32px;
+border-radius: 8px;
+`
   
 const Button = (props) => {  
   return (
-    <button
-      className="dribbble-button"
-    >	
+    <Buttonbase>	
       {props.label}
-    </button>
+    </Buttonbase>
   )
 
 }
 
 export default Button;
-
-
-
-
